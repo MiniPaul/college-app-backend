782d23af0e35fb1de91bb97faf5427dd385d0fe4
MiniPaul <minipaul1227@gmail.com>
2024-02-12 23:39:37 +0530

    college app backend login  branch created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose =require("mongoose")
 const cors =require("cors")
 
 const studentRouter =require("./controller/StudentRouter")
+const markRouter =require("./controller/MarkRouter")
 
 const app =express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/api/student",studentRouter)
+app.use("/api/mark",markRouter)
 
 mongoose.connect("mongodb+srv://minipaul:minipaul@cluster0.isuura7.mongodb.net/StudentDb?retryWrites=true&w=majority",
 {useNewUrlParser:true})
